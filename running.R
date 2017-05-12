2ae60a1a7e19d65b38a3a6ccdd63f453fbab59c0
skeydan <keydana@gmx.de>
2017-05-12 22:15:36 +0200

    check data quality

--- running.R
+++ running.R
@@ -3,12 +3,39 @@ library(dplyr)
 library(ggplot2)
 library(GGally)
 library(gridExtra)
+library(dlm)
+
+form <- function(theta){
+  dlmModPoly(order = 1, dV = exp(theta[1]), dW = exp(theta[2]))
+}
+
 
 df <- read_csv('tracker.csv')
 df
 
+var <- ts(df$Latitude)
+var <- ts(df$Longitude)
+var <- Nile
+model <- form(dlmMLE(var, parm = c(1, 1), form)$par)
+model
+filtered <- dlmFilter(var, model)
+
+#model <- form(dlmMLE(Nile, parm = c(1, 1), form)$par)
+#filtered <- dlmFilter(Nile, model)
+#ggfortify:::autoplot.dlmFiltered(filtered, ts.linetype = 'dashed', fitted.colour = 'blue')
+autoplot(filtered, ts.linetype = 'dashed', fitted.colour = 'cyan')
+
+smoothed <- dlmSmooth(filtered)
+class(smoothed)
+autoplot(smoothed)
+
+p <- autoplot(filtered, fitted.colour = 'cyan')
+autoplot(smoothed, ts.colour = 'blue', p = p)
+
+
+
 #df <- df %>% select(Stnr, Timestamp, Duration, Longitude, Latitude, LastLongitude, LastLatitude, Distance2D, Speed2D) 
-df <- df %>% select(Stnr, Timestamp, Duration, Distance2D, Distance2DTot, Speed2D, Altitude, LastAltitude) %>%
+df <- df %>% select(Stnr, Timestamp, Duration, Longitude, Latitude, Distance2D, Distance2DTot, Speed2D, Altitude, LastAltitude) %>%
   mutate(AltDiff = Altitude - LastAltitude, AltDiff2 = AltDiff^2, AltDiff3 = AltDiff^3, AltDiffExp = exp(AltDiff))
 #df <- df[70:80, ]
 df <- df %>% mutate(Stnr = factor(Stnr))
@@ -44,27 +71,6 @@ df529 <- df %>% filter(Stnr == 529)
 dfs <- list(df69, df395, df428, df529)
 dfs
 
-# Duration ~ Distance2D + AltDiff
-
-fits <- Map(function (df) lm(Duration ~ Distance2D + AltDiff, data = df), dfs)
-
-summaries <- Map(summary, fits)
-summaries
-
-plots_dist_by_duration <- Map(function (df) ggplot(df, aes(x = Distance2D, y = Duration, color = AltDiff)) + geom_point() +
-  scale_colour_gradient(low = "orange", high = "blue"), dfs)
-plots_duration_by_dist <- Map(function(df) ggplot(df, aes(x = Duration, y = Distance2D, color = AltDiff)) + geom_point() +
-  scale_colour_gradient(low = "orange", high = "blue"), dfs)
-
-do.call('grid.arrange', list('grobs' = plots_dist_by_duration, 'ncol' = 2, top = "Duration by Distance2D"))
-do.call('grid.arrange', list('grobs' = plots_duration_by_dist, 'ncol' = 2, top = "Duration by Distance2D"))
-
-for (df in dfs) print(ggpairs(df[c("Distance2D", "AltDiff", "Duration")]))
-
-
-
-#####################################################################################
-
 # Speed2D ~ Distance2DTot + AltDiff
 
 fits <- Map(function (df) lm(Speed2D ~ Distance2DTot + AltDiff, data = df), dfs)
@@ -96,20 +102,16 @@ do.call('grid.arrange', list('grobs' = prediction_plots, 'ncol' = 2, top = "Spee
 #####################################################################################
 
 #
-plots_speed_by_altdiff <- Map(function (df) ggplot(df, aes(x = AltDiff, y = Speed2D, color = Distance2DTot)) + geom_point() +
-                                scale_colour_gradient(low = "orange", high = "blue"), dfs)
+plots_speed_by_altdiff <- Map(function (df) ggplot(df, aes(x = AltDiff, y = Speed2D)) + geom_point(color = 'green'), dfs)
 do.call('grid.arrange', list('grobs' = plots_speed_by_altdiff, 'ncol' = 2, top = "Speed2D by AltDiff"))
 
-plots_speed_by_altdiff2 <- Map(function (df) ggplot(df, aes(x = AltDiff2, y = Speed2D, color = Distance2DTot)) + geom_point() +
-                                scale_colour_gradient(low = "orange", high = "blue"), dfs)
+plots_speed_by_altdiff <- Map(function (df) ggplot(df, aes(x = AltDiff2, y = Speed2D)) + geom_point(color = 'blue'), dfs)
 do.call('grid.arrange', list('grobs' = plots_speed_by_altdiff2, 'ncol' = 2, top = "Speed2D by AltDiff2"))
 
-plots_speed_by_altdiff3 <- Map(function (df) ggplot(df, aes(x = AltDiff3, y = Speed2D, color = Distance2DTot)) + geom_point() +
-                                scale_colour_gradient(low = "orange", high = "blue"), dfs)
+plots_speed_by_altdiff <- Map(function (df) ggplot(df, aes(x = AltDiff3, y = Speed2D)) + geom_point(color = 'red'), dfs)
 do.call('grid.arrange', list('grobs' = plots_speed_by_altdiff3, 'ncol' = 2, top = "Speed2D by AltDiff3"))
 
-plots_speed_by_altdiffexp <- Map(function (df) ggplot(df, aes(x = AltDiffExp, y = Speed2D, color = Distance2DTot)) + geom_point() +
-                                scale_colour_gradient(low = "orange", high = "blue"), dfs)
+plots_speed_by_altdiff <- Map(function (df) ggplot(df, aes(x = AltDiffExp, y = Speed2D)) + geom_point(color = 'cyan'), dfs)
 do.call('grid.arrange', list('grobs' = plots_speed_by_altdiffexp, 'ncol' = 2, top = "Speed2D by AltDiffExp"))
 
 
@@ -120,10 +122,40 @@ df529 %>% filter(AltDiff > 30)
 
 
 
+#####################################################################################
+
+#
+
+route <- read_csv('route.csv') 
+route
+route <- route %>% filter(RouteNumber == 1) %>% select(Longitude:Distance)
+route
 
 
+ggplot(route, aes(Distance, Altitude)) + geom_line() + 
+  geom_line(mapping = aes(x = Distance2DTot, y = Altitude), data = df69, color = 'blue') +
+  geom_line(mapping = aes(x = Distance2DTot, y = Altitude), data = df395, color = 'green') +
+  geom_line(mapping = aes(x = Distance2DTot, y = Altitude), data = df428, color = 'red') +
+  geom_line(mapping = aes(x = Distance2DTot, y = Altitude), data = df529, color = 'violet') 
 
+ggplot(route, aes(Latitude, Longitude)) + geom_line() + 
+  geom_line(mapping = aes(Latitude, Longitude), data = df69, color = 'blue') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df395, color = 'green') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df428, color = 'yellow') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df529, color = 'violet') 
 
+ggplot(route, aes(Latitude, Longitude)) + geom_line() + 
+  geom_line(mapping = aes(Latitude, Longitude), data = df69, color = 'blue') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df395, color = 'green') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df428, color = 'yellow') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df529, color = 'violet') +
+  coord_cartesian(xlim = c(46.320, 46.330), ylim = c(7.200, 7.225))
 
+ggplot(route, aes(Latitude, Longitude)) + geom_line() + 
+  geom_line(mapping = aes(Latitude, Longitude), data = df69, color = 'blue') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df395, color = 'green') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df428, color = 'yellow') +
+  geom_line(mapping = aes(Latitude, Longitude), data = df529, color = 'violet') +
+  coord_cartesian(xlim = c(46.350, 46.355), ylim = c(7.225, 7.250))
 
 
